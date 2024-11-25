d8cb0fdab9eb3b2192f9f6aa65862c83e68663cc
kzl <17166779@qq.com>
2024-11-25 15:31:30 +0800

    add sourcemap

--- index.js
+++ index.js
@@ -2,48 +2,54 @@ var deasync = require("deasync");
 var terser = require("terser");
 
 function syncPromise(fn) {
-  return function () {
-    var done = false;
-    var args = Array.prototype.slice.apply(arguments);
-    var err;
-    var res;
+    return function () {
+        var done = false;
+        var args = Array.prototype.slice.apply(arguments);
+        var err;
+        var res;
 
-    var ret = fn.apply(this, args);
+        var ret = fn.apply(this, args);
 
-    if (!ret || !ret.then) {
-      throw new Error("It's not a promise.");
-    }
+        if (!ret || !ret.then) {
+            throw new Error("It's not a promise.");
+        }
 
-    ret.then(function (result) {
-      done = true;
-      res = result;
-    });
+        ret.then(function (result) {
+            done = true;
+            res = result;
+        });
 
-    ret.catch(function (e) {
-      done = true;
-      err = e;
-    });
+        ret.catch(function (e) {
+            done = true;
+            err = e;
+        });
 
-    deasync.loopWhile(function () {
-      return !done;
-    });
-    if (err) throw err;
+        deasync.loopWhile(function () {
+            return !done;
+        });
+        if (err) throw err;
 
-    return res;
-  };
+        return res;
+    };
 }
+
 var syncMinify = syncPromise(terser.minify);
 
 module.exports = function (content, file, defaultOptions) {
-  var options = Object.assign({}, defaultOptions);
+    var options = Object.assign({}, defaultOptions);
 
-  delete options.filename;
+    delete options.filename;
 
-  var result = syncMinify(content, options);
+    var result = syncMinify(content, options);
 
-  // todo 支持 sourceMap
-
-  return result.code;
+    if (result.map) {
+        let mapping = file.wrap(file.dirname + '/' + file.filename + file.rExt + '.map');
+        mapping.setContent(result.map);
+        file.extras = file.extras || {};
+        file.extras.derived = file.extras.derived || [];
+        file.extras.derived.push(mapping);
+    }
+    return result.code;
 };
 
 module.exports.defaultOptions = {};
